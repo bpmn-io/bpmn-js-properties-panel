a3e270b3bb15440e1af4b4304be0eafb0643d5ef
pedesen <patrick.dehn@camunda.com>
2015-11-11 13:44:13 +0100

    chore(test): improve coding style in TestHelper

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -7,62 +7,77 @@ TestHelper.insertCSS('bpmn-embedded.css', require('bpmn-js/assets/bpmn-font/css/
 TestHelper.insertCSS('properties.css', require('../assets/properties.css'));
 
 TestHelper.insertCSS('diagram-js-testing.css',
-  '.test-container .result { height: auto; }' + '.bjs-container { height: 400px !important; }' + ' div.test-container {height: auto}'
+  '.test-container .result { height: auto; }' +
+  '.bjs-container { height: 400px !important; }' +
+  ' div.test-container {height: auto}'
 );
 
-var propertiesPanelModule = require('../lib'),
-  domQuery = require('min-dom/lib/query');
+var domQuery = require('min-dom/lib/query');
 
 /**
- * Triggers an change event
+ * Triggers a change event
+ *
  * @param element on which the change should be triggered
  * @param eventType type of the event (e.g. click, change, ...)
  */
 var triggerEvent = function(element, eventType) {
+
   var evt;
-  if(!!document.createEvent) {
+
+  eventType = eventType || 'change';
+
+  if (!!document.createEvent) {
     try {
       // Chrome, Safari, Firefox
-      evt = new MouseEvent(( eventType || 'change' ), { view: window, bubbles: true, cancelable: true });
+      evt = new MouseEvent((eventType), { view: window, bubbles: true, cancelable: true });
     } catch (e) {
       // IE 11, PhantomJS (wat!)
       evt = document.createEvent('MouseEvent');
-      evt.initEvent(( eventType || 'change' ), true, true);
+
+      evt.initEvent((eventType), true, true);
     }
     return element.dispatchEvent(evt);
   } else {
     // Welcome IE
     evt = document.createEventObject();
+
     return element.fireEvent('on' + eventType, evt);
   }
-
 };
 
 var triggerValue = function(element, value, eventType) {
   element.value = value;
+
   this.triggerEvent(element, eventType);
 };
 
 var triggerInput = function(element, value) {
   element.value = value;
+
   this.triggerEvent(element, 'input');
+
   element.focus();
 };
 
 /**
  *  Select the option with the given value
+ *
  *  @param element contains the options
  *  @param optionValue value which should be selected
  */
 var selectedByOption = function(element, optionValue) {
+
   var options = domQuery.all('option', element);
-    for(var i = 0; i< options.length; i++) {
-      var option = options[i];
-      if(option.value === optionValue) {
-        element.selectedIndex = i;
-        break;
-      }
+
+  for(var i = 0; i< options.length; i++) {
+
+    var option = options[i];
+
+    if(option.value === optionValue) {
+      element.selectedIndex = i;
+      break;
     }
+  }
 };
 
 module.exports.triggerEvent = triggerEvent;
