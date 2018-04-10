40935240b4ef01bfd435f7c20d594cbea1976832
Philipp Fromme <philippfromme@outlook.com>
2018-04-10 16:03:31 +0200

    fix(properties): show properties of label target when selecting label
    Related to camunda/camunda-modeler#731

--- test/spec/provider/camunda/CamundaPropertiesProviderSpec.js
+++ test/spec/provider/camunda/CamundaPropertiesProviderSpec.js
@@ -18,8 +18,6 @@ var propertiesPanelModule = require('../../../../lib'),
 
 describe('camunda-properties', function() {
 
-  var diagramXML = require('./CamundaPropertiesProvider-JobConfiguration.bpmn');
-
   var testModules = [
     coreModule,
     selectionModule,
@@ -34,62 +32,114 @@ describe('camunda-properties', function() {
     container = TestContainer.get(this);
   });
 
-  beforeEach(bootstrapModeler(diagramXML, {
-    modules: testModules,
-    moddleExtensions: {
-      camunda: camundaModdlePackage
-    }
-  }));
+  
+  describe('job configuration', function() {
+
+    var diagramXML = require('./CamundaPropertiesProvider-JobConfiguration.bpmn');
+
+    beforeEach(bootstrapModeler(diagramXML, {
+      modules: testModules,
+      moddleExtensions: {
+        camunda: camundaModdlePackage
+      }
+    }));
 
 
-  beforeEach(inject(function(commandStack, propertiesPanel) {
+    beforeEach(inject(function(commandStack, propertiesPanel) {
 
-    var undoButton = document.createElement('button');
-    undoButton.textContent = 'UNDO';
+      var undoButton = document.createElement('button');
+      undoButton.textContent = 'UNDO';
 
-    undoButton.addEventListener('click', function() {
-      commandStack.undo();
-    });
+      undoButton.addEventListener('click', function() {
+        commandStack.undo();
+      });
 
-    container.appendChild(undoButton);
+      container.appendChild(undoButton);
 
-    propertiesPanel.attachTo(container);
-  }));
-
-
-  it('should hide the job configuration group when all items are hidden or empty', inject(function(propertiesPanel, selection, elementRegistry) {
-
-    // given
-    var shape = elementRegistry.get('ServiceTask_1'),
-        selector = '[data-group=jobConfiguration]';
-
-    // when
-    selection.select(shape);
-
-    var group = domQuery(selector, propertiesPanel._container);
-
-    // then
-    expect(domClasses(group).has('bpp-hidden')).to.be.true;
-  }));
+      propertiesPanel.attachTo(container);
+    }));
 
 
-  it('should show the job configuration group when there is a non hidden group element', inject(function(propertiesPanel, selection, elementRegistry) {
-
-    var shape = elementRegistry.get('ServiceTask_1'),
-        groupSelector = '[data-group=jobConfiguration]',
-        inputSelector = 'div[data-entry=asyncBefore] input[name=asyncBefore]';
+    it('should hide the job configuration group when all items are hidden or empty', inject(function(propertiesPanel, selection, elementRegistry) {
 
       // given
-    selection.select(shape);
-
-    var asyncBeforeCheckbox = domQuery(inputSelector, propertiesPanel._container);
-
+      var shape = elementRegistry.get('ServiceTask_1'),
+          selector = '[data-group=jobConfiguration]';
+  
       // when
-    TestHelper.triggerEvent(asyncBeforeCheckbox, 'click');
-    var group = domQuery(groupSelector, propertiesPanel._container);
-
+      selection.select(shape);
+  
+      var group = domQuery(selector, propertiesPanel._container);
+  
       // then
-    expect(domClasses(group).has('bpp-hidden')).to.be.false;
-  }));
+      expect(domClasses(group).has('bpp-hidden')).to.be.true;
+    }));
+  
+  
+    it('should show the job configuration group when there is a non hidden group element', inject(function(propertiesPanel, selection, elementRegistry) {
+  
+      var shape = elementRegistry.get('ServiceTask_1'),
+          groupSelector = '[data-group=jobConfiguration]',
+          inputSelector = 'div[data-entry=asyncBefore] input[name=asyncBefore]';
+  
+        // given
+      selection.select(shape);
+  
+      var asyncBeforeCheckbox = domQuery(inputSelector, propertiesPanel._container);
+  
+      // when
+      TestHelper.triggerEvent(asyncBeforeCheckbox, 'click');
+      var group = domQuery(groupSelector, propertiesPanel._container);
+  
+      // then
+      expect(domClasses(group).has('bpp-hidden')).to.be.false;
+    }));
+
+  });
+
+
+  describe('external labels', function() {
+
+    var diagramXML = require('./ExternalLabels.bpmn');
+
+    beforeEach(bootstrapModeler(diagramXML, {
+      modules: testModules,
+      moddleExtensions: {
+        camunda: camundaModdlePackage
+      }
+    }));
+
+
+    beforeEach(inject(function(commandStack, propertiesPanel) {
+
+      var undoButton = document.createElement('button');
+      undoButton.textContent = 'UNDO';
+
+      undoButton.addEventListener('click', function() {
+        commandStack.undo();
+      });
+
+      container.appendChild(undoButton);
+
+      propertiesPanel.attachTo(container);
+    }));
+
+
+    it('should show properties of label target when selecting external label', inject(
+      function(elementRegistry, propertiesPanel, selection) {
+        var shape = elementRegistry.get('SequenceFlow_1_label'),
+            groupSelector = '[data-group=details]';
+  
+        // given
+        selection.select(shape);
+  
+        // then
+        var group = domQuery(groupSelector, propertiesPanel._container);
+
+        expect(domClasses(group).has('bpp-hidden')).to.be.false;
+      })
+    );
+
+  });
 
 });
