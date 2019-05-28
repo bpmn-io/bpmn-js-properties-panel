146fcd549755cac082710a4af1953db891205cc7
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-05-28 14:00:48 +0200

    chore(test): use original style sheets

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -7,8 +7,8 @@ var domQuery = require('min-dom').query,
     domAttr = require('min-dom').attr;
 
 
-TestHelper.insertCSS('diagram-js.css', require('bpmn-js/dist/assets/diagram-js.css'));
-TestHelper.insertCSS('bpmn-embedded.css', require('bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'));
+TestHelper.insertCSS('diagram-js.css', require('diagram-js/assets/diagram-js.css'));
+TestHelper.insertCSS('bpmn-embedded.css', require('bpmn-font/dist/css/bpmn-embedded.css'));
 TestHelper.insertCSS('properties.css', require('./assets/properties.css'));
 
 TestHelper.insertCSS('diagram-js-testing.css',
