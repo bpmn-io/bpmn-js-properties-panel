51dd639c3f278f3c0cb2d089bd3ac305d61e4e7c
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-09-30 13:42:11 +0200

    feat(factory): provide toggle switch component
    Related to #365

---
+++ test/spec/factory/ToggleSwitchEntryFactorySpec.js
@@ -0,0 +1,39 @@
+var EntryFactory = require('lib/factory/EntryFactory'),
+    ToggleSwitchEntry = EntryFactory.toggleSwtich;
+
+
+describe('factory/ToggleSwitchEntry', function() {
+
+  describe('rendering', function() {
+
+    it('should render', function() {
+
+      // when
+      var html = ToggleSwitchEntry({
+        id: 'foo',
+        label: 'label',
+        labelOff: 'off',
+        labelOn: 'on',
+        descriptionOff: 'this is off',
+        descriptionOn: 'this is on',
+        modelProperty: 'toggle'
+      }).html;
+
+
+      // then
+      expect(html).to.eql('<label for="foo" >label</label>' +
+      '<div class="bpp-field-wrapper">' +
+        '<label class="bpp-toggle-switch__switcher">' +
+          '<input id="foo" type="checkbox" name="toggle" />' +
+          '<span class="bpp-toggle-switch__slider"></span>' +
+        '</label>'+
+        '<p class="bpp-toggle-switch__label" data-show="isOn">on</p>' +
+        '<p class="bpp-toggle-switch__label" data-show="isOff">off</p>' +
+      '</div>' +
+      '<div class="bpp-field-description"data-show="isOn">this is on</div>' +
+      '<div class="bpp-field-description"data-show="isOff">this is off</div>');
+    });
+
+  });
+
+});
