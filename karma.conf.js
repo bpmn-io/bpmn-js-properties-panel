f56bee64414431fe95179cefbd8990eae511fd5d
Nico Rehwaldt <git_nikku@nixis.de>
2021-03-01 13:04:24 +0100

    test: remove legacy CI hack / test using ChromeHeadless per default

--- karma.conf.js
+++ karma.conf.js
@@ -1,25 +1,12 @@
 var path = require('path');
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 var absoluteBasePath = path.resolve(__dirname);
 
 // configures browsers to run test against
 // any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
-var browsers =
-  (process.env.TEST_BROWSERS || 'PhantomJS')
-    .replace(/^\s+|\s+$/, '')
-    .split(/\s*,\s*/g)
-    .map(function(browser) {
-      if (browser === 'ChromeHeadless') {
-        process.env.CHROME_BIN = require('puppeteer').executablePath();
-
-        // workaround https://github.com/GoogleChrome/puppeteer/issues/290
-        if (process.platform === 'linux') {
-          return 'ChromeHeadless_Linux';
-        }
-      }
-
-      return browser;
-    });
+var browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(/,/g);
 
 
 module.exports = function(karma) {
@@ -42,17 +29,6 @@ module.exports = function(karma) {
     reporters: [ 'spec' ],
 
     browsers: browsers,
-
-    customLaunchers: {
-      ChromeHeadless_Linux: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox'
-        ],
-        debug: true
-      }
-    },
 
     client: {
       mocha: {
