fd972a47a608d392dd08dace6d88cb509719b21a
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-10-21 19:12:35 +0200

    test: verify assets are copied

--- test/distro/distroSpec.js
+++ test/distro/distroSpec.js
@@ -2,6 +2,10 @@ const {
   expect
 } = require('chai');
 
+const fs = require('fs'),
+      path = require('path');
+
+const DIST_DIR = path.join(__dirname, '../../dist');
 
 describe('modules', function() {
 
@@ -13,7 +17,7 @@ describe('modules', function() {
       BpmnPropertiesProviderModule,
       ZeebePropertiesProviderModule,
       useService
-    } = require('../../dist');
+    } = require(DIST_DIR);
 
     // then
     expect(BpmnPropertiesPanelModule).to.exist;
@@ -23,3 +27,27 @@ describe('modules', function() {
     expect(useService).to.exist;
   });
 });
+
+
+describe('assets', function() {
+
+
+
+  it('should expose properties panel styles', verifyExists('assets/properties-panel.css'));
+
+
+  it('should expose element templates styles', verifyExists('assets/element-templates.css'));
+
+
+});
+
+function verifyExists(relativePath) {
+  return function() {
+
+    // given
+    const filePath = path.join(DIST_DIR, relativePath);
+
+    // then
+    expect(fs.existsSync(filePath), `file ${relativePath} does not exist`).to.be.true;
+  };
+}
