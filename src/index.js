cd7abee25cdcfdf859440136ed2c4b0ce4a3a998
Philipp <philippfromme@outlook.com>
2021-10-28 11:01:22 +0200

    feat: export providers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 export { default as BpmnPropertiesPanelModule } from './render';
 export { default as BpmnPropertiesProviderModule } from './provider/bpmn';
 export { default as ZeebePropertiesProviderModule } from './provider/zeebe';
+export { default as CamundaPlatformPropertiesProviderModule } from './provider/camunda-platform';
+export { default as ElementTemplatesPropertiesProviderModule } from './provider/element-templates';
 
 // hooks
 export { useService } from './hooks';
