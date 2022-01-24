c4b565df6f861205f6df63a3e265375e0b10d4e7
Max Tru <maximilian.trumpf@camunda.com>
2022-01-24 15:34:26 +0100

    chore: export DescriptionProvider as ZeebeDescriptionProvider
    related to camunda/camunda-bpmn-js#83

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { default as BpmnPropertiesProviderModule } from './provider/bpmn';
 export { default as ZeebePropertiesProviderModule } from './provider/zeebe';
 export { default as CamundaPlatformPropertiesProviderModule } from './provider/camunda-platform';
 export { default as ElementTemplatesPropertiesProviderModule } from './provider/element-templates';
+export { DescriptionProvider as ZeebeDescriptionProvider } from './contextProvider/zeebe';
 
 // hooks
 export { useService } from './hooks';
