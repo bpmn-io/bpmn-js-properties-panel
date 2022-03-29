fb4fa4deee54d44693f0cf0ea44a3c4fb03df04a
Nico Rehwaldt <git_nikku@nixis.de>
2022-03-29 15:04:36 +0200

    test(cloud-element-templates): verify basic propery retention and removal

--- test/spec/provider/cloud-element-templates/ElementTemplates.spec.js
+++ test/spec/provider/cloud-element-templates/ElementTemplates.spec.js
@@ -1,17 +1,15 @@
 import TestContainer from 'mocha-test-container-support';
 
 import {
-  isAny
-} from 'bpmn-js/lib/util/ModelUtil';
-
-import {
   bootstrapModeler,
   createCanvasEvent as canvasEvent,
-  inject
+  inject,
+  getBpmnJS
 } from 'test/TestHelper';
 
 import {
-  getBusinessObject
+  getBusinessObject,
+  is
 } from 'bpmn-js/lib/util/ModelUtil';
 
 import coreModule from 'bpmn-js/lib/core';
@@ -23,6 +21,10 @@ import zeebeModdlePackage from 'zeebe-bpmn-moddle/resources/zeebe';
 import diagramXML from './ElementTemplates.bpmn';
 
 import templates from './fixtures/simple';
+
+import compatibilityDiagramXML from './fixtures/compatibility.bpmn';
+
+import compatibilityTemplates from './fixtures/compatibility';
 
 
 describe('provider/cloud-element-templates - ElementTemplates', function() {
@@ -195,27 +197,186 @@ describe('provider/cloud-element-templates - ElementTemplates', function() {
 
   });
 
+});
+
+
+describe('provider/cloud-element-templates - ElementTemplates', function() {
+
+  let container;
+
+  beforeEach(function() {
+    container = TestContainer.get(this);
+  });
+
+  beforeEach(bootstrapModeler(compatibilityDiagramXML, {
+    container: container,
+    modules: [
+      coreModule,
+      elementTemplatesModule,
+      modelingModule,
+      {
+        propertiesPanel: [ 'value', { registerProvider() {} } ]
+      }
+    ],
+    moddleExtensions: {
+      zeebe: zeebeModdlePackage
+    }
+  }));
+
+  beforeEach(inject(function(elementTemplates) {
+    elementTemplates.set(compatibilityTemplates);
+  }));
+
 
   describe('applyTemplate', function() {
+
+    function expectTemplate(element, template) {
+
+      return getBpmnJS().invoke(function(elementTemplates) {
+        expect(elementTemplates.get(element)).to.eql(template);
+
+        const bo = getBusinessObject(element);
+        expect(bo.get('zeebe:modelerTemplate')).to.eql(template && template.id);
+        expect(bo.get('zeebe:modelerTemplateVersion')).to.eql(template && template.version);
+      });
+    }
+
+    function getInputBinding(element, target) {
+
+      const bo = getBusinessObject(element);
+
+      const extensionElements = bo.get('extensionElements');
+
+      if (!extensionElements) {
+        return null;
+      }
+
+      const ioMapping = extensionElements.values.find(function(extension) {
+        return is(extension, 'zeebe:IoMapping');
+      });
+
+      return (
+        ioMapping && ioMapping.inputParameters.find(function(parameter) {
+          return parameter.target === target;
+        }) || {}
+      ).source;
+    }
+
 
     it('should set template on element', inject(function(elementRegistry, elementTemplates) {
 
       // given
-      const task = elementRegistry.get('Task_1');
+      const task = elementRegistry.get('NON_TASK');
 
       const template = elementTemplates.getAll().find(
-        t => isAny(task, t.appliesTo)
+        t => t.id === 'test.Basic'
       );
 
       // assume
       expect(template).to.exist;
+      expect(elementTemplates.get(task)).not.to.exist;
 
       // when
       const updatedTask = elementTemplates.applyTemplate(task, template);
 
       // then
       expect(updatedTask).to.exist;
-      expect(elementTemplates.get(updatedTask)).to.equal(template);
+      expectTemplate(updatedTask, template);
+    }));
+
+
+    describe('should replace template on element', function() {
+
+      it('keep compatible attribute', inject(
+        function(elementRegistry, elementTemplates) {
+
+          // given
+          const task = elementRegistry.get('BASIC_V1');
+          const aValue = getInputBinding(task, 'a');
+
+          const basicV2Template = elementTemplates.getAll().find(
+            t => t.id === 'test.Basic' && t.version === 2
+          );
+
+          // assume
+          expect(basicV2Template).to.exist;
+          expect(aValue).to.eql('= abba');
+          expect(elementTemplates.get(task)).to.exist;
+
+          // when
+          const updatedTask = elementTemplates.applyTemplate(task, basicV2Template);
+
+          // then
+          expect(updatedTask).to.exist;
+          expectTemplate(updatedTask, basicV2Template);
+
+          // <a> is kept
+          expect(getInputBinding(task, 'a'), '<a> kept').to.eql(aValue);
+
+          // <b> is established
+          expect(getInputBinding(task, 'b'), '<b> added').to.eql('b');
+        }
+      ));
+
+
+      it('remove incompatible attribute', inject(
+        function(elementRegistry, elementTemplates) {
+
+          // given
+          const task = elementRegistry.get('BASIC_V2');
+          const bValue = getInputBinding(task, 'b');
+
+          const otherTemplate = elementTemplates.getAll().find(
+            t => t.id === 'test.Other'
+          );
+
+          // assume
+          expect(otherTemplate).to.exist;
+          expect(bValue).to.eql('= beatles');
+          expect(elementTemplates.get(task)).to.exist;
+
+          // when
+          const updatedTask = elementTemplates.applyTemplate(task, otherTemplate);
+
+          // then
+          expect(updatedTask).to.exist;
+          expectTemplate(updatedTask, otherTemplate);
+
+          // <a> (incompatible) is removed
+          expect(getInputBinding(task, 'a'), '<a> removed').not.to.exist;
+
+          // <b> (compatible) is kept
+          expect(getInputBinding(task, 'b'), '<b> kept').to.eql(bValue);
+        }
+      ));
+
+    });
+
+
+    it('should remove template from element', inject(function(elementRegistry, elementTemplates) {
+
+      // given
+      const task = elementRegistry.get('BASIC_V2');
+      const aValue = getInputBinding(task, 'a');
+      const bValue = getInputBinding(task, 'b');
+
+      // assume
+      expect(elementTemplates.get(task)).to.exist;
+      expect(aValue).to.exist;
+      expect(bValue).to.exist;
+
+      // when
+      const updatedTask = elementTemplates.applyTemplate(task, null);
+
+      // then
+      expect(updatedTask).to.exist;
+
+      // template unlinked
+      expectTemplate(updatedTask, null);
+
+      // attributes are kept
+      expect(getInputBinding(task, 'a')).to.eql(aValue);
+      expect(getInputBinding(task, 'b')).to.eql(bValue);
     }));
 
   });
