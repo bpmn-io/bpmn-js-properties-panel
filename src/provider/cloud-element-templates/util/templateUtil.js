11b72fb009b97b48dae8e2c78e516ed79ca947be
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-04-01 13:54:56 +0200

    feat(cloud-element-templates/util): remove icon on unlink

--- src/provider/cloud-element-templates/util/templateUtil.js
+++ src/provider/cloud-element-templates/util/templateUtil.js
@@ -1,3 +1,9 @@
+import { findExtension } from '../Helper';
+
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+
+import { without } from 'min-dash';
+
 export {
   getVersionOrDateFromTemplate,
   removeTemplate
@@ -6,10 +12,22 @@ export {
 export function unlinkTemplate(element, injector) {
   const modeling = injector.get('modeling');
 
+  // (1) remove template attributes
   modeling.updateProperties(element, {
     'zeebe:modelerTemplate': null,
     'zeebe:modelerTemplateVersion': null
   });
+
+
+  // (2) remove template icon
+  const icon = findExtension(element, 'zeebe:ModelerTemplateIcon');
+
+  if (icon) {
+    const extensionElements = getBusinessObject(element).get('extensionElements');
+    modeling.updateModdleProperties(element, extensionElements, {
+      values: without(extensionElements.get('values'), extension => extension === icon)
+    });
+  }
 }
 
 export function updateTemplate(element, newTemplate, injector) {
