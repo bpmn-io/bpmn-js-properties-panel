9f2e113287bc4b3ee2c910a85bafecc097be260a
Philipp <philippfromme@outlook.com>
2022-04-20 14:05:28 +0200

    test: add user task forms integration tests

--- test/spec/provider/camunda-platform/FormTypeProps.spec.js
+++ test/spec/provider/camunda-platform/FormTypeProps.spec.js
@@ -24,6 +24,8 @@ import CoreModule from 'bpmn-js/lib/core';
 import SelectionModule from 'diagram-js/lib/features/selection';
 import ModelingModule from 'bpmn-js/lib/features/modeling';
 
+import BehaviorsModule from 'camunda-bpmn-js-behaviors/lib/camunda-platform';
+
 import BpmnPropertiesPanel from 'src/render';
 
 import BpmnPropertiesProvider from 'src/provider/bpmn';
@@ -42,7 +44,8 @@ describe('provider/camunda-platform - FormTypeProps', function() {
     CamundaPlatformPropertiesProvider,
     CoreModule,
     ModelingModule,
-    SelectionModule
+    SelectionModule,
+    BehaviorsModule
   ];
 
   const moddleExtensions = {
@@ -290,6 +293,54 @@ describe('provider/camunda-platform - FormTypeProps', function() {
 
     expect(select.value).to.equal('');
   }));
+
+
+  describe('integration', function() {
+
+    it('should remove form key when form ref is set', inject(async function(elementRegistry, selection) {
+
+      // given
+      const task = elementRegistry.get('StartEvent_FormKey'),
+            businessObject = getBusinessObject(task);
+
+      await act(() => {
+        selection.select(task);
+      });
+
+      // when
+      const select = domQuery('select[name=formType]', container);
+
+      changeInput(select, 'formRef');
+
+      // then
+      expect(businessObject.get('camunda:formKey')).not.to.exist;
+      expect(businessObject.get('camunda:formRef')).to.equal('');
+    }));
+
+
+    it('should remove form ref when form key is set', inject(async function(elementRegistry, selection) {
+
+      // given
+      const task = elementRegistry.get('StartEvent_FormRef'),
+            businessObject = getBusinessObject(task);
+
+      await act(() => {
+        selection.select(task);
+      });
+
+      // when
+      const select = domQuery('select[name=formType]', container);
+
+      changeInput(select, 'formKey');
+
+      // then
+      expect(businessObject.get('camunda:formRef')).not.to.exist;
+      expect(businessObject.get('camunda:formRefBinding')).not.to.exist;
+      expect(businessObject.get('camunda:formRefVersion')).not.to.exist;
+      expect(businessObject.get('camunda:formKey')).to.equal('');
+    }));
+
+  });
 
 });
 
