a802a5bff6a72d81914abcd32e976353ef6b5906
Martin Stamm <martin.stamm@camunda.com>
2023-06-13 13:44:04 +0200

    chore: remove element templates

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ export { default as BpmnPropertiesPanelModule } from './render';
 export { default as BpmnPropertiesProviderModule } from './provider/bpmn';
 export { default as ZeebePropertiesProviderModule } from './provider/zeebe';
 export { default as CamundaPlatformPropertiesProviderModule } from './provider/camunda-platform';
-export { default as CloudElementTemplatesPropertiesProviderModule } from './provider/cloud-element-templates';
-export { default as ElementTemplatesPropertiesProviderModule } from './provider/element-templates';
 export { DescriptionProvider as ZeebeDescriptionProvider } from './contextProvider/zeebe';
 
 // hooks
 export { useService } from './hooks';
-
-// utils
-export { Validator as CloudElementTemplatesValidator } from './provider/cloud-element-templates/Validator';
--- src/provider/cloud-element-templates/create/index.js
+++
@@ -1,8 +0,0 @@
-import TemplateElementFactory from './TemplateElementFactory';
-
-export default {
-  __init__: [
-    'templateElementFactory'
-  ],
-  templateElementFactory: [ 'type', TemplateElementFactory ]
-};
