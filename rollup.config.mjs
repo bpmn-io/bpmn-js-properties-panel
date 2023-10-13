d21f0f6f561927b09c758e3ac3fa3ee194c46a7e
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-10-13 13:42:01 +0200

    chore: resolve icons via babel

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,7 @@
-import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import json from '@rollup/plugin-json';
-import reactSvg from 'rollup-plugin-react-svg';
 import resolve from '@rollup/plugin-node-resolve';
 
 import {
@@ -56,20 +54,8 @@ export default [
 function pgl(plugins = []) {
   return [
     ...plugins,
-    alias({
-      entries: [
-        { find: 'react', replacement: 'preact/compat' }
-      ]
-    }),
-    reactSvg(),
     babel({
-      babelHelpers: 'bundled',
-      plugins: [
-        [ '@babel/plugin-transform-react-jsx', {
-          'importSource': 'preact',
-          'runtime': 'automatic'
-        } ]
-      ]
+      babelHelpers: 'bundled'
     }),
     json(),
     resolve({
