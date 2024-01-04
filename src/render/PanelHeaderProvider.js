75bd12a4c925d2f4a239e8d4a2a0f457343ea703
zxuanhong <z7630853@163.com>
2024-01-04 23:18:20 +0800

    feat: modify panel header icon render, to make it consistent with element template icon renderer

--- src/render/PanelHeaderProvider.js
+++ src/render/PanelHeaderProvider.js
@@ -190,7 +190,9 @@ function getTemplateDocumentation(element, elementTemplates) {
 }
 
 function getModelerTemplateIcon(element) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const config = useService('config.elementTemplateIconRenderer', false);
+  
   const { iconProperty = 'zeebe:modelerTemplateIcon'} = config;
 
   return getBusinessObject(element).get(iconProperty);
