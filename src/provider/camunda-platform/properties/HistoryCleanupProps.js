8d6bc4977073ac142fdf9535637204bcfd4539ef
Prajwol Bhandari <prajwol.bhandari@fmr.com>
2024-02-12 11:05:27 -0700

    feat(properties/HistoryCleanupProps): Add user document hint for history time to live for bpmn
    Related to https://github.com/camunda/camunda-modeler/issues/4062

--- src/provider/camunda-platform/properties/HistoryCleanupProps.js
+++ src/provider/camunda-platform/properties/HistoryCleanupProps.js
@@ -58,6 +58,11 @@ function HistoryTimeToLive(props) {
     element,
     id: 'historyTimeToLive',
     label: translate('Time to live'),
+    description: <div>
+      <a href="https://docs.camunda.org/manual/7.20/update/minor/719-to-720/#enforce-history-time-to-live" target="_blank" rel="noopener" title={ translate('Time to live documentation') }>
+        { translate('Learn more about time to live.') }
+      </a>
+    </div>,
     getValue,
     setValue,
     debounce
