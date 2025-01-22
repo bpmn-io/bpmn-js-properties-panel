2d0334f59e6d89f43bac682050def34a0e2f2b94
Jarek Danielak <jarek.danielak@camunda.com>
2025-01-22 16:13:15 +0100

    feat: support ad-hoc subprocesses
    Related to https://github.com/camunda/camunda-modeler/issues/4739
    
    deps: update to `zeebe-bpmn-moddle@1.9.0`

---
+++ src/provider/zeebe/properties/ActiveElementsProps.js
@@ -0,0 +1,110 @@
+import {
+  is,
+  getBusinessObject
+} from 'bpmn-js/lib/util/ModelUtil';
+
+import { isFeelEntryEdited } from '@bpmn-io/properties-panel';
+
+import { useService } from 'src/hooks';
+
+import { FeelEntryWithVariableContext } from 'src/entries/FeelEntryWithContext';
+
+import {
+  getExtensionElementsList,
+  addExtensionElements
+} from 'src/utils/ExtensionElementsUtil';
+
+import { createElement } from 'src/utils/ElementUtil';
+
+export function ActiveElementsProps(props) {
+  const {
+    element
+  } = props;
+
+  if (!is(element, 'bpmn:AdHocSubProcess')) {
+    return [];
+  }
+
+  const entries = [
+    {
+      id: 'activeElementsCollection',
+      component: ActiveElementsCollection,
+      isEdited: isFeelEntryEdited
+    }
+  ];
+
+  return entries;
+}
+
+function ActiveElementsCollection(props) {
+  const {
+    element
+  } = props;
+
+  const commandStack = useService('commandStack');
+  const bpmnFactory = useService('bpmnFactory');
+  const translate = useService('translate');
+  const debounce = useService('debounceInput');
+
+  const getValue = () => {
+    return getProperty(element);
+  };
+
+  const setValue = (value) => {
+    return setProperty(element, value, commandStack, bpmnFactory);
+  };
+
+  return FeelEntryWithVariableContext({
+    element,
+    id: 'activeElements-activeElementsCollection',
+    label: translate('Active elements collection'),
+    feel: 'required',
+    getValue,
+    setValue,
+    debounce
+  });
+}
+
+function getProperty(element) {
+  const extensionElement = getExtensionElement(element);
+  return extensionElement && extensionElement.get('activeElementsCollection');
+}
+
+function setProperty(element, value, commandStack, bpmnFactory) {
+
+  const extensionElement = getExtensionElement(element);
+
+  if (!extensionElement) {
+
+    // (1) create extension element
+    const adHoc = createElement(
+      'zeebe:AdHoc',
+      {
+        activeElementsCollection: value
+      },
+      undefined,
+      bpmnFactory
+    );
+
+    const businessObject = getBusinessObject(element);
+    addExtensionElements(element, businessObject, adHoc, bpmnFactory, commandStack);
+
+  } else {
+
+    // (2) update extension element's property
+    commandStack.execute('element.updateModdleProperties', {
+      element,
+      moddleElement: extensionElement,
+      properties: {
+        activeElementsCollection: value
+      }
+    });
+  }
+}
+
+function getExtensionElement(element) {
+  const businessObject = getBusinessObject(element);
+  const extensions = getExtensionElementsList(businessObject, 'zeebe:AdHoc');
+  return extensions[0];
+}
+
