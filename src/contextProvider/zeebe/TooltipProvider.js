7e69454c00cedd6f8bc998b2432be2196b7ed906
Jarek Danielak <jarek.danielak@camunda.com>
2026-01-23 10:51:49 +0100

    chore: remove empty tooltip

--- src/contextProvider/zeebe/TooltipProvider.js
+++ src/contextProvider/zeebe/TooltipProvider.js
@@ -387,13 +387,7 @@ const TooltipProvider = {
           </a>        </p>
       </div>
     );
-  },
-  'group-event-condition': (element) => {
-
-    // const translate = useService('translate');
-
-    // TODO(@jarekdanielak): Link to Conditional Events docs when available
-  },
+  }
 };
 
 export default TooltipProvider;
