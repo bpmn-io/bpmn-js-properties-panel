db5ef941c6a087985777f3cccf0c975ae9d0ba05
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-19 15:35:24 +0100

    test: extract essential test helpers into helper

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -12,6 +12,11 @@ import {
   insertCSS
 } from 'bpmn-js/test/helper';
 
+import {
+  query as domQuery,
+  matches as domMatches
+} from 'min-dom';
+
 import semver from 'semver';
 
 import fileDrop from 'file-drops';
@@ -105,6 +110,55 @@ export function clickInput(input) {
 
 export function mouseEnter(element) {
   fireEvent.mouseEnter(element);
+}
+
+export function findGroup(container, id) {
+  return domQuery(`[data-group-id="group-${id}"`, container);
+}
+
+export function queryField(elementName, container) {
+  const selector = `[name=${elementName}]`;
+
+  return domQuery(selector, container);
+}
+
+export function findField(elementName, container) {
+  const element = queryField(elementName, container);
+
+  expect(element, `DOM element matching <[name=${elementName}]>`).to.exist;
+
+  return element;
+}
+
+export async function setFieldValue(element, value) {
+
+  // plain input
+  if (domMatches(element, 'input')) {
+    return changeInput(element, value);
+  }
+
+  // CodeMirror editor
+  const editorElement = domQuery('[role="textbox"]', element);
+  return setEditorValue(editorElement, value);
+}
+
+export function fieldValue(element) {
+
+  // plain input
+  if (domMatches(element, 'input')) {
+    return element.value;
+  }
+
+  // CodeMirror editor
+  if (domMatches(element, 'div')) {
+    const editorElement = domQuery('[role="textbox"]', element);
+
+    expect(editorElement, '[role="textbox"] element').to.exist;
+
+    return editorElement.textContent;
+  }
+
+  throw new Error('unsupported element - ' + element);
 }
 
 export function insertCoreStyles() {
